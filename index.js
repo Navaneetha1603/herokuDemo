5aba1585031e2439e7aeb939631558d537966303
Navaneetha1603 <navaneethakandlapelli@gmail.com>
2020-12-14 22:29:36 +0530

    commited

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ app.get("/",(req,res)=>{
 app.get("/hello",(req,res)=>{
    res.send("hello")
 })
-app.get("/htmlpage",(req,res)=>{
-    res.sendFile(path.join(__dirname,"public/index.html"));
-})
-app.get("/someaction",(req,res)=>{
-    console.log(req.query);
-    let data=req.query.uname;
-    console.log(data);
-    res.send(`username is ${data}`);
-})
+// app.get("/htmlpage",(req,res)=>{
+//     res.sendFile(path.join(__dirname,"public/index.html"));
+// })
+// app.get("/someaction",(req,res)=>{
+//     console.log(req.query);
+//     let data=req.query.uname;
+//     console.log(data);
+//     res.send(`username is ${data}`);
+// })
 app.get("/getDelete",(request,response)=>
 {
     var obj=[{
